95e9e48322a7d3070a9f2d8ac5d61e2a50ae2df9
Ionut Pop <pop.ianosd@gmail.com>
2025-05-26 20:20:01 +0300

    update dockerfile and requirements

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,7 @@
 docker run \
     -p 80:80 \
     -v $PWD/app_data:/app/data \
-    -e EPAPER_DATA=/app/data ianosd/pax-avize:latest
+    -e FIREBIRD_DSN="localhost:/var/lib/firebird/data/CONT_BAZA.FDB" \
+    -e FIREBIRD_USER="sysdba" \
+    -e FIREBIRD_PASSWORD="password" \
+    ianosd/pax-avize:latest

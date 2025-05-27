42e000769ba3fea6322342be933c1d59e51db153
Ionut Pop <pop.ianosd@gmail.com>
2025-05-27 17:34:17 +0300

    fix stoc interogation

--- backend/saga_daq.py
+++ backend/saga_daq.py
@@ -11,16 +11,8 @@ class Product:
     denumire: str
     um: str
     tva: float
-    den_tip: str
     stoc: float
-    grupa: str
-    pret_vanz: float
     pret_v_tva: float
-    is_valuta: bool
-    cod_bare: str
-    cod_fe: str
-    cod_cpv: str
-    plu: str
 
 
 def get_product_by_code(code):
@@ -33,7 +25,7 @@ def get_product_by_code(code):
     cursor = conn.cursor()
 
     # Query the product by code
-    cursor.execute("SELECT * FROM ARTICOLE WHERE cod = ?", (code,))
+    cursor.execute("SELECT cod,denumire,um,tva,stoc,pret_v_tva FROM ARTICOLE WHERE cod = ?", (code,))
     row = cursor.fetchone()
     conn.close()
 
@@ -46,14 +38,6 @@ def get_product_by_code(code):
         denumire=row[1],
         um=row[2],
         tva=float(row[3]),
-        den_tip=row[4],
-        stoc=float(row[5]),
-        grupa=row[6],
-        pret_vanz=row[7],
-        pret_v_tva=float(row[8]),
-        is_valuta=bool(row[9]),
-        cod_bare=row[10],
-        cod_fe=row[11],
-        cod_cpv=row[12],
-        plu=row[13]
+        stoc=float(row[4]),
+        pret_v_tva=float(row[5])
     )
--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 docker run \
     -p 80:80 \
-    -v $PWD/app_data:/app/data \
-    -e FIREBIRD_DSN="localhost:/var/lib/firebird/data/CONT_BAZA.FDB" \
+    -v $PWD/misc/server_data:/app/data \
+    -e FIREBIRD_DSN="192.168.0.118:/var/lib/firebird/data/CONT_BAZA.FDB" \
     -e FIREBIRD_USER="sysdba" \
-    -e FIREBIRD_PASSWORD="password" \
+    -e FIREBIRD_PASSWORD="mastersaga" \
     ianosd/pax-avize:latest
